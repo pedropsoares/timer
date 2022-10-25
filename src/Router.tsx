1b353eeb72c2a746271869fe7951db878e4865b2
C1325737 Pedro Paulo dos Santos Soares <c1325737@interno.bb.com.br>
2022-10-25 15:20:12 -0300

    history implemented

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { Home } from './pages/Home'
-import { History } from './pages/History/History'
+import { History } from './pages/History'
 import { DefaultLayout } from './layout/DefaultLayout'
 
 export const Router = () => {
--- src/pages/history/History.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const History = () => {
-  return <h1>History</h1>
-}
