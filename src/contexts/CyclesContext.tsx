b2c7ac23bc5077ad58f24ad17f390ac9e872c290
Pedro Soares <pedropaulos.1998@gmail.com>
2022-12-28 17:35:28 -0300

    saving cycle in localStorage

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useState, useReducer } from 'react'
+import React, { createContext, ReactNode, useState, useReducer, useEffect } from 'react'
 import { addNewCycleAction, interruptCurrentCycleAction, marckCurrentCycleAsFineshedAction } from '../reducers/cycles/actions'
 import { ActionTypes, Cycle, cyclesReduce } from '../reducers/cycles/reducer'
 
@@ -31,9 +31,21 @@ export const CyclesContesxtProvider = ({
   const [cyclesState, dispatch] = useReducer(cyclesReduce, {
     cycles: [],
     activeCycleId: null
+  }, () => {
+    const storedStateAsJSON = localStorage.getItem('@ignite-timer:cycles-state-1.0.0')
+
+    if (storedStateAsJSON) {
+      return JSON.parse(storedStateAsJSON)
+    }
   })
 
   const [amountSecondsPassed, setAmountSecondsPassed] = useState<number>(0)
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
+  }, [cyclesState])
 
   const { cycles, activeCycleId } = cyclesState
   const activeCycles = cycles.find(({ id }) => id === activeCycleId)
